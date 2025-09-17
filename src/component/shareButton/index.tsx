4146ac3e90b4ebd10c79587e7c56087d8c39041a
witch49 <47845837+witch49@users.noreply.github.com>
2025-09-17 11:55:42 +0900

    initial commit

---
+++ src/component/shareButton/index.tsx
@@ -0,0 +1,79 @@
+import {
+  BRIDE_FULLNAME,
+  GROOM_FULLNAME,
+  LOCATION,
+  SHARE_ADDRESS,
+  SHARE_ADDRESS_TITLE,
+  WEDDING_DATE,
+} from "../../const"
+import ktalkIcon from "../../icons/ktalk-icon.png"
+import { LazyDiv } from "../lazyDiv"
+import { useKakao } from "../store"
+
+const baseUrl = import.meta.env.BASE_URL
+
+export const ShareButton = () => {
+  const kakao = useKakao()
+  return (
+    <LazyDiv className="footer share-button">
+      <button
+        className="ktalk-share"
+        onClick={() => {
+          if (!kakao) {
+            return
+          }
+
+          kakao.Share.sendDefault({
+            objectType: "location",
+            address: SHARE_ADDRESS,
+            addressTitle: SHARE_ADDRESS_TITLE,
+            content: {
+              title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
+              description:
+                WEDDING_DATE.format("YYYY년 MMMM D일 dddd A h시") +
+                "\n" +
+                LOCATION,
+              imageUrl:
+                window.location.protocol +
+                "//" +
+                window.location.host +
+                baseUrl +
+                "/preview_image.png",
+              link: {
+                mobileWebUrl:
+                  window.location.protocol +
+                  "//" +
+                  window.location.host +
+                  baseUrl,
+                webUrl:
+                  window.location.protocol +
+                  "//" +
+                  window.location.host +
+                  baseUrl,
+              },
+            },
+            buttons: [
+              {
+                title: "초대장 보기",
+                link: {
+                  mobileWebUrl:
+                    window.location.protocol +
+                    "//" +
+                    window.location.host +
+                    baseUrl,
+                  webUrl:
+                    window.location.protocol +
+                    "//" +
+                    window.location.host +
+                    baseUrl,
+                },
+              },
+            ],
+          })
+        }}
+      >
+        <img src={ktalkIcon} alt="ktalk-icon" /> 카카오톡으로 공유하기
+      </button>
+    </LazyDiv>
+  )
+}
---
+++ src/const.ts
@@ -0,0 +1,76 @@
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone"
+import "dayjs/locale/ko"
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.locale("ko")
+
+export { dayjs }
+
+export const WEDDING_DATE = dayjs.tz("2026-01-10 12:30", "Asia/Seoul")
+export const HOLIDAYS = [15]
+
+export const LOCATION = "성균관컨벤션웨딩홀"
+export const LOCATION_ADDRESS = "서울특별시 종로구 성균관로 31, 성균관대학교 정문옆"
+
+export const SHARE_ADDRESS = LOCATION
+export const SHARE_ADDRESS_TITLE = LOCATION
+
+export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
+
+export const NMAP_PLACE_ID = 13321741
+export const KMAP_PLACE_ID = 8634826
+
+export const BRIDE_FULLNAME = "정ㅇㅇ"
+export const BRIDE_FIRSTNAME = "ㅇㅇ"
+export const BRIDE_TITLE = "딸"
+export const BRIDE_FATHER = "정ㅇㅇ"
+export const BRIDE_MOTHER = "박ㅇㅇ"
+export const BRIDE_INFO = [
+  {
+    relation: "신부",
+    name: BRIDE_FULLNAME,
+    phone: "010-0000-0000",
+    account: "우리은행 0000000000000",
+  },
+  {
+    relation: "신부 아버지",
+    name: BRIDE_FATHER,
+    phone: "010-0000-0000",
+    account: "하나은행 00000000000",
+  },
+  {
+    relation: "신부 어머니",
+    name: BRIDE_MOTHER,
+    phone: "010-0000-0000",
+    account: "하나은행 00000000000000",
+  },
+]
+
+export const GROOM_FULLNAME = "노ㅇㅇ"
+export const GROOM_FIRSTNAME = "ㅇㅇ"
+export const GROOM_TITLE = "아들"
+export const GROOM_FATHER = "노ㅇㅇ"
+export const GROOM_MOTHER = "채ㅇㅇ"
+export const GROOM_INFO = [
+  {
+    relation: "신랑",
+    name: GROOM_FULLNAME,
+    phone: "010-0000-0000",
+    account: "하나은행 00000000000000",
+  },
+  {
+    relation: "신랑 아버지",
+    name: GROOM_FATHER,
+    phone: "010-0000-0000",
+    account: "신한은행 000000000000",
+  },
+  {
+    relation: "신랑 어머니",
+    name: GROOM_MOTHER,
+    phone: "010-0000-0000",
+    account: "국민은행 000000000000",
+  },
+]
