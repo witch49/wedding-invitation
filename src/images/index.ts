252ecf6bac493393f05099225f9bd24519b1c7cd
witch49 <47845837+witch49@users.noreply.github.com>
2025-09-22 11:13:02 +0900

    img test

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,5 +1,5 @@
-import coverImage from "./cover.png"
-import image1 from "./image1.png"
+import coverImage from "./cover.jpg"
+import image1 from "./image1.jpg"
 import image2 from "./image2.png"
 import image3 from "./image3.png"
 import image4 from "./image4.png"
