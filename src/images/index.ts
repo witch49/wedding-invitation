84b8fbe5c74024b5e49c5c15983327f03347a958
witch49 <47845837+witch49@users.noreply.github.com>
2025-09-22 11:25:49 +0900

    update img

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,6 +1,6 @@
 import coverImage from "./cover.jpg"
 import image1 from "./image1.jpg"
-import image2 from "./image2.png"
+import image2 from "./image2.jpg"
 import image3 from "./image3.png"
 import image4 from "./image4.png"
 import image5 from "./image5.png"
